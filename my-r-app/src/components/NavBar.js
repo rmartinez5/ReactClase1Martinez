de91574fc95a33cf717141ff11463c98f5de1032
Romina Martinez <martinezrominapaola@gmail.com>
2022-05-02 23:34:14 -0300

    clase jsx

---
+++ my-r-app/src/components/NavBar.js
@@ -0,0 +1,17 @@
+// import './App.css';
+
+function NavBar() {
+  return (
+    <div>
+        <h1>NAVBAR</h1>
+        <nav class="header__nav">
+            <a href=""> Inicio </a>
+            <a href=""> Repuestos</a>
+            <a href=""> Galería</a>
+            <a href=""> Contacto</a>
+        </nav>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ react/my-r-app/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+        <h1>NAVBAR</h1>
+    </div>
+  );
+}
+
+export default NavBar;
